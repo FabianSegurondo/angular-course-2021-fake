1c8516e1fbc8b3989dd775a95a8a1e423872522c
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-08-10 18:22:46 -0400

    08/10 second task, name and last name connected using banana box

---
+++ src/app/test3/test3.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test3',
+  templateUrl: './test3.component.html',
+  styleUrls: ['./test3.component.css']
+})
+export class Test3Component implements OnInit {
+  name;
+  lastname;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  fullname(event) {
+    console.log('Fullname:  ', this.name + ' ' + this.lastname);
+  }
+}
