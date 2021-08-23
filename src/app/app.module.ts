69e3f86c5bc2c6b5371e0bb17f68ce9441ca1b1d
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-08-23 18:14:27 -0400

    secondcheckpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { BilleterasComponent } from './billeteras/billeteras.component';
-import { TransaccionesComponent } from './transacciones/transacciones.component';
+import { Directive1Directive } from './directive1.directive';
+import { Directive2Directive } from './directive2.directive';
+import { FirstTestComponent } from './first-test/first-test.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,BilleterasComponent, TransaccionesComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+
+    Directive1Directive,
+    Directive2Directive,
+    FirstTestComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
