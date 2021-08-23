9ded09b4688457a6a460da0167597668ce9ee00f
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-08-23 18:01:24 -0400

    firstcheckpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,249 +1,34 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import {of} from 'rxjs';
-import {BehaviorSubject, Subscription} from 'rxjs';
-import { filter, map,delay } from 'rxjs/operators';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+  
 })
-export class AppComponent {
-  title = 'angular2021';
-  myStatus = 'my status';
+export class AppComponent  {
+  allWallets = [
 
-  sw = true;
+    {wallet: "MARIA123", name: "maria", eth: 0, btc: 2},
+    {wallet: "JUAN123", name: "juan", eth: 5, btc: 0},
+    {wallet: "LUCAS123", name: "lucas", eth: 6, btc: 3},
+    {wallet: "MARCOS123", name: "marcos", eth: 0, btc: 2},
+    {wallet: "PEDRO123", name: "pedro", eth: 1, btc: 0},
+    {wallet: "JUANA123", name: "juana", eth: 10, btc: 12}
+    ];
 
-  color:string;
+allTransactions = [
+  {date: "2019-09-07T15:50+00Z ", from: "MARIA123", to:"JUANA123", quantity: 2, moneyType: "btc", mineType: "PoW", miner: 5},
+  {date: "2019-09-07T15:50+00Z ", from: "JUAN123", to: "PEDRO123", quantity: 2, moneyType: "eth", mineType: "PoS", miner: 21},
+  {date: "2019-09-07T15:50+00Z ", from: "LUCAS123", to: "MARCOS123", quantity: 2, moneyType: "btc", mineType: "PoW", miner: 5},
+  {date: "2019-09-07T15:50+00Z ", from: "MARCOS123", to: "LUCAS123", quantity: 2, moneyType: "eth", mineType: "PoS", miner: 10},
+  {date: "2019-09-07T15:50+00Z ", from: "PEDRO123", to: "JUAN123", quantity: 2, moneyType: "btc", mineType: "PoW", miner: 5},
+  {date: "2019-09-07T15:50+00Z ", from: "JUANA123", to: "MARIA123", quantity: 2, moneyType: "eth", mineType: "PoS", miner: 30},
+  {date: "2019-09-07T15:50+00Z ", from: "MARIA123", to: "JUANA123", quantity: 2, moneyType: "btc", mineType: "PoW", miner: 2},
+  {date: "2019-09-07T15:50+00Z ", from: "JUAN123", to: "PEDRO123", quantity: 2, moneyType: "eth", mineType: "PoS", miner: 15},
+  {date: "2019-09-07T15:50+00Z ", from: "LUCAS123", to: "MARCOS123", quantity: 2, moneyType: "btc", mineType: "PoW", miner: 3},
+  {date: "2019-09-07T15:50+00Z ", from: "MARCOS123", to: "LUCAS123", quantity: 2, moneyType: "eth", mineType: "PoS", miner: 5}
+  ]
 
-  people = [
-    { name: 'felipe', lastName: 'felipao' },
-    { name: 'achito', lastName: 'del ozo' },
-    { name: 'maximus', lastName: 'salazn' },
-    { name: 'sebita', lastName: 'blacky' }
-  ];
-
-  tictock = of([1,2,3,4,5]);
-  personaAsub:Subscription;
-  video = 1;
-  ticktock2 = new BehaviorSubject(this.video);
-
-  @ViewChild ('myDiv1' ) myDiv1: ElementRef;
-
-  @ViewChild ('myDiv2' ) myDiv2: ElementRef;
-  
-  @ViewChild ('myCompFabo' ) myCompFabo: any;
-  constructor() {
-
-    //ejemplo de tictoc que vemos con of, subscribe se ejcuta el observable
-    //cada persona que se suscribe recibe videos
-
-    //persona A
-    this.tictock.pipe(
-      map(s => s.join('-')),
-      map(s => s + 'hola'),
-    ).subscribe(v => {
-      console.log('PERSON A VIDEO', v);
-    });
-   
-    //persona B
-    this.tictock.pipe(
-      filter((v:any) => v[0]%2 === 1)
-    ).subscribe(v => {
-      console.log('PERSON B VIDEO', v);
-    });
-     //persona C
-     this.tictock.subscribe(v => {
-      console.log('PERSONA C VIDEO', v)
-    });
-
-    //ahora con los ejemplos de observer para tictoc y con behavior
-    // PERSON A
-    this.personaAsub = this.ticktock2.pipe(
-      filter(s => s%2 === 0)
-    ).subscribe(v => {
-      console.log('PERSON A VIDEO', v);
-    });
-    // PERSON B
-    this.tictock.pipe(
-      delay(4000)
-    ).subscribe(v => {
-      console.log('PERSON B VIDEO', v);
-    });
-    // PERSON C
-    this.tictock.subscribe(v => {
-      console.log('PERSON C VIDEO', v);
-    });
-    
-
-
-
-    const testMap = [1, 2, 3, 4, 5, 6].map(item => item * 2);
-    console.log(testMap);
-
-    const testForeeach = [1, 2, 3, 4, 5, 6].forEach(item => item);
-    console.log(testMap);
-
-    const testFind = [1, 2, 3, 4, 5, 6].find(item => item === 4); //tres iguales para igualar de manera estricta, compara todo, si no dos iguales basta
-    console.log(testFind);
-
-    const testFilter = [1, 2, 3, 4, 5, 6].filter(item => item % 2 === 0);
-    console.log(testFilter); //para ver si numero es par o no
-    //sirve para edades
-
-    const testFindIndex = [1, 2, 3, 4, 5, 6].findIndex(item => item % 2 === 90);
-    console.log(testFindIndex);
-    //nos devuelve toda la posicion y busca en todo el array
-
-    const testIndexOf = [90, 2, 300, 4, 5, 6].indexOf(300);
-    console.log(testIndexOf);
-    //trabaja tambvien con cadenas y la posicion
-
-    // const testIndexOf = 'ricardo'.indexOf('o')
-    // console.log(testIndexOf);
-
-    const testJoin = [1, 2, 3, 4, 5, 6].join(',');
-    console.log(testJoin);
-
-    const testSplit = '1,2,3,4,5,6'.split(',');
-    console.log(testSplit);
-
-    const testSpliceA = [10, 20, 30, 40, 50, 60];
-    const testSpliceB = [1, 2, 3, 4, 5, 6].splice(1, 2);
-    testSpliceA.splice(0, 1);
-    console.log(testSpliceB, testSpliceA);
-
-    //operador reduce
-
-    //sirve para sumar, apartir de array
-    //recorre cada una de las posicioned
-    //value = valor actual
-    //acc = aculador de la suma anterior, tambvien puede multiplicar
-    //podemos pasar valor inicial del acumulador
-    //se puede poner un parametro como el valor inicial como el tercer parametro
-    const testReduce = [1, 2, 3, 4, 5, 6].reduce(
-      (acc, value) => acc + value,
-      100
-    );
-    console.log(testReduce);
-  
-
-   //para convertir json a array
-  const testEntries = {value: 'ricardo', key:'RPC'};
-  console.log('aaaaa', Object.entries(testEntries));
-
-
-
-  //para el segindo key
-  const testKey = {value: 'ricardo', key:'RPC'};
-  console.log('bbbb', Object.keys(testEntries));
-    
-  //tambien se puede hacer lo mismo para mostrar todos los valores
-  //OJO PREGUNTA EXAMEN
-  //TENEMOS
-  // const asasdad = [1:'d',2; 'd',3;'b',4,5,6,7,8,9,4];
-  //dado esto convertir a array, es un json
-//solo quiero los atruvutos
-//obj.keys
-//quiero los nombre
-//obj.values en un array
-
-
-//variable let 
-//solo se ejecuta en el contexto definido, solo dentro del constructor
-//por ejemplo se la puede usar en un for
-//y se la puede usar en multiples for, por que es una constante en el contextp
-
-let testLet
-
-const array1 = [1,2,3,4,5];
-const array2 = [9,10,11, ... array1]
-//el spread operator hace esto 
-//tambien reemplaza valorrs
-//tamboen sirve para concatenar
-
-const per1 = {name : 'a', age :12};
-const per2 = {data: 'R', ...per1};
-
-//en redux siempre se usa el spread operator
-console.log('Spread', array2, per2)
-
-
-
-//destructuracion
-//destrucutra constantes de un JSON, saca partes del json que necesito usar, se convierte en una variable
-
-const per3 = {
-  name: 'a', 
-age: 12, 
-phone: 12323234234, 
-extra: 123, 
-response:200,
-response2:200,
-response4:200
-};
-
-const {phone:ci} = per3;
-console.log('name=', ci); 
-
-
-//{1:'a',2:'a',3:'a',4:'a',5:'a',6:'a'} convertir a un array y sumar los numeros pares
-
-//[1,2,3,4,5,6] filtrar los numeros impares y mostrarlos como cadena
-
-//ejercicio 1 
-
-const a = {1:'a', 2:'a', 3:'a',4:'a',5:'a',6:'a'};
-const b = Object.keys(a).map(n => parseInt(n)).reduce ((acc, value) => {
-  if(value %2 !== 1){
-    acc = acc + value;
-
-  }
-  return acc;
-}, 0);
-console.log('aaa', b)
-
-
-//ejercicio 2
-
-console.log ('222= ' , [1,2,3,4,5,6].filter (s => s%2 !==0).join (','))
-
-
-}
-
-printDataFabianseguComp(event:any){
-  console.log('FABIANSEGU COMP:', event);
-}
-
-printData(event){
-  console.log('CHILD COMP SEND DATA: ', event);
-}
-
-
-onAddVideo(){
-  this.video ++
-  this.ticktock2.next(this.video); 
-}
-person1Unsubscribe(){
-  this.personaAsub.unsubscribe();
-  console.log('PERSON A SE DESUSCRIBE')
-}
-test (event){
-  console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,', event);
-}
-
-
-
-test100(event){
-  console.log('234234234234234', event);
-}
-onShowLocalVars(){
-  console.log(this.myDiv1, this.myDiv2, this.myCompFabo);
-    //accedmos directamente al valor del child
-
-  this.myCompFabo.onClickTest();
-  this.myDiv1.nativeElement.style.backgroundColor ='green';
-  console.log(this.myDiv1, this.myDiv2, this.myCompFabo);
-  this.myDiv2.nativeElement.value = 'fabito';
-}
+quantEth= this.allWallets.reduce((acc,value) => acc+value.eth, 0) 
+quantBtc= this.allWallets.reduce((acc,value) => acc+value.btc, 0) 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,15 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { Directive1Directive } from './directive1.directive';
-import { Directive2Directive } from './directive2.directive';
-import { Module1Module } from './module1/module1.module';
-import { AdminModule } from './pages/admin/admin.module';
-import { BuyModule } from './pages/buy/buy.module';
-import { HomeModule } from './pages/home/home.module';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
-import { Test3Component } from './test3/test3.component';
+import { HelloComponent } from './hello.component';
+import { BilleterasComponent } from './billeteras/billeteras.component';
+import { TransaccionesComponent } from './transacciones/transacciones.component';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    Directive1Directive,
-    Directive2Directive
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    Module1Module,
-    HomeModule,
-    AdminModule,
-    BuyModule,
-    CoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent,BilleterasComponent, TransaccionesComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
