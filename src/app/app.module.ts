5fd6d629302d4cbc09ccc7370b13244d6793abaf
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-08-30 18:54:00 -0400

    08/30 second task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
 
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', redirectTo: 'pages', pathMatch: 'full'},
   {path: 'login', loadChildren:
       () => import('./login/login.module').then(m => m.LoginModule)},
   {path: 'pages', loadChildren:
       () => import('./pages/pages.module').then(m => m.PagesModule)}
 ];
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes)
   ],
- 
   providers: [],
   bootstrap: [AppComponent]
 })
