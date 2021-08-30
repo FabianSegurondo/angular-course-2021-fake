0ce2a8c5c9fce3aa48a82d44ea7c90d2668fb42d
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-08-30 18:31:54 -0400

    08/30 first task, configuration of ngbootstrap

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgbButtonsModule, NgbModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
 
 const routes: Routes = [
   {path: '', component: LoginComponent}
 ]
-
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
+    NgbToastModule,
+    NgbButtonsModule,
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ]
 })
